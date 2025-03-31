a63545cdf98a84fe5daa749618c6f152447e2994
salvobono <salvatorebono2001@gmail.com>
2025-03-31 22:35:20 +0200

    add previewList

--- src/app/news/news.component.ts
+++ src/app/news/news.component.ts
@@ -1,11 +1,37 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-news',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './news.component.html',
-  styleUrl: './news.component.css'
+  styleUrl: './news.component.css',
 })
 export class NewsComponent {
-
+  gamePreviewList = [
+    {
+      img: 'images/astrobot.jpg',
+      title: 'Astrobot',
+    },
+    {
+      img: 'images/spider-man.jpg',
+      title: 'Spider-Man 2',
+    },
+    {
+      img: 'images/2k25.jpg',
+      title: '2K25',
+    },
+    {
+      img: 'images/horizon.jpg',
+      title: 'Horizon',
+    },
+    {
+      img: '/images/helldivers.jpg',
+      title: 'Helldivers II',
+    },
+    {
+      img: '/images/ronin.jpg',
+      title: 'Rise of The Ronin',
+    },
+  ];
 }
