080224c37b53f82e328aa69f05c8fd6588190db8
salvobono <salvatorebono2001@gmail.com>
2025-04-01 16:27:21 +0200

    add takeUntilDestroyed for best practice

--- src/app/browse/browse.component.ts
+++ src/app/browse/browse.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, DestroyRef, OnInit } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { Game } from '../models/game.model';
 import { GamesService } from '../services/games.service';
 
@@ -12,11 +13,18 @@ import { GamesService } from '../services/games.service';
 export class BrowseComponent implements OnInit {
   games: Game[] = [];
 
-  constructor(private gameService: GamesService) {}
+  constructor(
+    private gameService: GamesService,
+    private destroyRef: DestroyRef
+  ) {}
 
+  // takeUntilDestroyed fa in modo di eliminare il subscribe una volta cambiata pagina
   ngOnInit(): void {
-    this.gameService.getGames().subscribe((games) => {
-      this.games = games;
-    });
+    this.gameService
+      .getGames()
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe((games) => {
+        this.games = games;
+      });
   }
 }
