2dfae336784b9100a753f96240652a7f71bbdd9f
salvobono <salvatorebono2001@gmail.com>
2025-04-01 10:21:32 +0200

    add img and section "altri giochi"

--- src/app/news/news.component.ts
+++ src/app/news/news.component.ts
@@ -10,28 +10,63 @@ import { Component } from '@angular/core';
 export class NewsComponent {
   gamePreviewList = [
     {
-      img: 'images/astrobot.jpg',
+      img: 'images/game-preview/astrobot.jpg',
       title: 'Astrobot',
     },
     {
-      img: 'images/spider-man.jpg',
+      img: 'images/game-preview/spider-man.jpg',
       title: 'Spider-Man 2',
     },
     {
-      img: 'images/2k25.jpg',
+      img: 'images/game-preview/2k25.jpg',
       title: '2K25',
     },
     {
-      img: 'images/horizon.jpg',
+      img: 'images/game-preview/horizon.jpg',
       title: 'Horizon',
     },
     {
-      img: '/images/helldivers.jpg',
+      img: 'images/game-preview/helldivers.jpg',
       title: 'Helldivers II',
     },
     {
-      img: '/images/ronin.jpg',
+      img: 'images/game-preview/ronin.jpg',
       title: 'Rise of The Ronin',
+    },
+  ];
+
+  gameCategories = [
+    {
+      img: 'images/game-categories/ps5.jpg',
+      title: 'Tutti i giochi',
+    },
+    {
+      img: 'images/game-categories/enhanced-games.jpg',
+      title: 'Giochi Potenziati',
+    },
+    {
+      img: 'images/game-categories/promotions.jpg',
+      title: 'Promozioni',
+    },
+    {
+      img: 'images/game-categories/additional-content.jpg',
+      title: 'Contenuti aggiuntivi',
+    },
+    {
+      img: 'images/game-categories/free-to-play.jpg',
+      title: 'Free to Play',
+    },
+    {
+      img: 'images/game-categories/ps-vr2.jpg',
+      title: 'PS VR2',
+    },
+    {
+      img: 'images/game-categories/ps-plus.jpg',
+      title: 'Helldivers II',
+    },
+    {
+      img: 'images/game-categories/ps4.jpg',
+      title: 'Tutti i giochi',
     },
   ];
 }
