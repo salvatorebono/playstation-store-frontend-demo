c64de68fb3c8d806a0efddd9105cb29c76a1caee
salvobono <salvatorebono2001@gmail.com>
2025-04-01 21:32:15 +0200

    add carousel

---
+++ src/app/carousel/carousel.component.ts
@@ -0,0 +1,36 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-carousel',
+  imports: [CommonModule],
+  templateUrl: './carousel.component.html',
+  styleUrl: './carousel.component.css',
+})
+export class CarouselComponent {
+  @Input() items: any[] = [];
+
+  currentIndex = 0;
+  itemsPerPage = 6;
+
+  //visibleItems: Restituisce una fetta dell'array carouselItems, a partire da currentIndex e includendo itemsPerPage elementi.
+  get visibleItems() {
+    return this.items.slice(
+      this.currentIndex,
+      this.currentIndex + this.itemsPerPage
+    );
+  }
+
+  /*
+    Se il carosello ha 9 elementi (carouselItems.length = 9) e itemsPerPage = 6, la funzione imposterà currentIndex = 9 - 6 = 3
+    In questo modo, quando visibleItems viene eseguito, restituirà gli ultimi 6 elementi, che vanno dalla posizione 3 fino alla fine dell'array ([4, 5, 6, 7, 8, 9]).
+ */
+  next() {
+    this.currentIndex = this.items.length - this.itemsPerPage;
+  }
+
+  prev() {
+    // Imposta currentIndex a 0, mostrando sempre i primi elementi.
+    this.currentIndex = 0;
+  }
+}
--- src/app/news/news.component.ts
+++ src/app/news/news.component.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { CarouselComponent } from '../carousel/carousel.component';
 
 @Component({
   selector: 'app-news',
-  imports: [CommonModule],
+  imports: [CommonModule, CarouselComponent],
   templateUrl: './news.component.html',
   styleUrl: './news.component.css',
 })
@@ -122,28 +123,190 @@ export class NewsComponent {
       price: '€29,99',
     },
   ];
-
-  currentIndex = 0;
-  itemsPerPage = 6;
-
-  //visibleItems: Restituisce una fetta dell'array carouselItems, a partire da currentIndex e includendo itemsPerPage elementi.
-  get visibleItems() {
-    return this.carouselItems.slice(
-      this.currentIndex,
-      this.currentIndex + this.itemsPerPage
-    );
-  }
-
-  /*
-    Se il carosello ha 9 elementi (carouselItems.length = 9) e itemsPerPage = 6, la funzione imposterà currentIndex = 9 - 6 = 3
-    In questo modo, quando visibleItems viene eseguito, restituirà gli ultimi 6 elementi, che vanno dalla posizione 3 fino alla fine dell'array ([4, 5, 6, 7, 8, 9]).
- */
-  next() {
-    this.currentIndex = this.carouselItems.length - this.itemsPerPage;
-  }
-
-  prev() {
-    // Imposta currentIndex a 0, mostrando sempre i primi elementi.
-    this.currentIndex = 0;
-  }
+  carouselItems1 = [
+    {
+      img: 'images/gallery-carousel/11.jpg',
+      title: 'Assassin’s Creed Shadows',
+      price: '€79,99',
+    },
+    {
+      img: 'images/gallery-carousel/12.jpg',
+      title: 'The First Berserker: Khazan',
+      price: '€59,99',
+    },
+    {
+      img: 'images/gallery-carousel/13.jpg',
+      title: 'Split Fiction',
+      price: '€49,99',
+    },
+    {
+      img: 'images/gallery-carousel/14.jpg',
+      title: 'WWE 2K25',
+      price: '€74,99',
+    },
+    {
+      img: 'images/gallery-carousel/15.jpg',
+      title: 'Atomfall',
+      price: '€59,99',
+    },
+    {
+      img: 'images/gallery-carousel/16.jpg',
+      title: 'BLEACH Rebirth of Souls',
+      price: '€69,99',
+    },
+    {
+      img: 'images/gallery-carousel/17.jpg',
+      title: 'Age of Mythology: Retold',
+      price: '€29,99',
+    },
+    {
+      img: 'images/gallery-carousel/18.jpg',
+      title: 'AI LIMIT',
+      price: '€34,99',
+    },
+    {
+      img: 'images/gallery-carousel/19.jpg',
+      title: 'MLB® The Show™ 25',
+      price: '€69,99',
+    },
+    {
+      img: 'images/gallery-carousel/20.jpg',
+      title: 'Karma: The Dark World',
+      price: '€24,99',
+    },
+    {
+      img: 'images/gallery-carousel/21.jpg',
+      title: 'War Robots: Frontiers',
+      price: 'Gratis',
+    },
+    {
+      img: 'images/gallery-carousel/33.jpg',
+      title: 'The Last of Us™ Part II',
+      price: '€49,99',
+    },
+  ];
+  carouselItems2 = [
+    {
+      img: 'images/gallery-carousel/22.jpg',
+      title: 'F1® 25 Iconic Edition',
+      price: '€99,99',
+    },
+    {
+      img: 'images/gallery-carousel/23.jpg',
+      title: 'Forza Horizon 5 Premium Edition',
+      price: '€99,99',
+    },
+    {
+      img: 'images/gallery-carousel/24.jpg',
+      title: 'Forza Horizon 5 Standard Edition',
+      price: '€69,99',
+    },
+    {
+      img: 'images/gallery-carousel/25.jpg',
+      title: 'Indiana Jones e l’antico Cerchio',
+      price: '€79,99',
+    },
+    {
+      img: 'images/gallery-carousel/26.jpg',
+      title: 'Indiana Jones e l’antico Cerchio Premium Edition',
+      price: '€19,99',
+    },
+    {
+      img: 'images/gallery-carousel/27.jpg',
+      title: 'DEATH STRANDING 2: ON THE BEACH Digital Deluxe',
+      price: '€89,99',
+    },
+    {
+      img: 'images/gallery-carousel/28.jpg',
+      title: 'Clair Obscur: Expedition 33',
+      price: '€49,99',
+    },
+    {
+      img: 'images/gallery-carousel/29.jpg',
+      title: 'ELDEN RING NIGHTREIGN Deluxe Edition',
+      price: '€69,99',
+    },
+    {
+      img: 'images/gallery-carousel/30.jpg',
+      title: 'F1® 25 Standard Edition',
+      price: '€79,99',
+    },
+    {
+      img: 'images/gallery-carousel/32.jpg',
+      title: 'Days Gone Remastered',
+      price: '€49,99',
+    },
+    {
+      img: 'images/gallery-carousel/34.jpg',
+      title: 'METAL GEAR SOLID Δ: SNAKE EATER',
+      price: '€89,99',
+    },
+    {
+      img: 'images/gallery-carousel/35.jpg',
+      title: 'DOOM: The Dark Ages Premium Edition',
+      price: '€109,99',
+    },
+  ];
+  carouselItems3 = [
+    {
+      img: 'images/gallery-carousel/36.jpg',
+      title: 'Silent Operative Demo',
+      price: 'Gratis',
+    },
+    {
+      img: 'images/gallery-carousel/37.jpg',
+      title: 'Vagrus - The Riven Realms Prologue',
+      price: 'Gratis',
+    },
+    {
+      img: 'images/gallery-carousel/38.jpg',
+      title: 'Boxville 2',
+      price: 'Gratis',
+    },
+    {
+      img: 'images/gallery-carousel/39.jpg',
+      title: 'Balance Blox Demo',
+      price: 'Gratis',
+    },
+    {
+      img: 'images/gallery-carousel/40.jpg',
+      title: 'Quriocity',
+      price: 'Gratis',
+    },
+    {
+      img: 'images/gallery-carousel/41.jpg',
+      title: 'Tainted Grail: Fall of Avalon',
+      price: 'Gratis',
+    },
+    {
+      img: 'images/gallery-carousel/42.jpg',
+      title: 'Smells Like a Mushroom Demo',
+      price: 'Gratis',
+    },
+    {
+      img: 'images/gallery-carousel/43.jpg',
+      title: 'Galactic Guardian - DEMO',
+      price: 'Gratis',
+    },
+    {
+      img: 'images/gallery-carousel/44.jpg',
+      title: 'The Jumping Burger Rush - DEMO',
+      price: 'Gratis',
+    },
+    {
+      img: 'images/gallery-carousel/45.jpg',
+      title: 'Atelier Yumia: The Alchemist of Memories & the Envisioned Land',
+      price: 'Gratis',
+    },
+    {
+      img: 'images/gallery-carousel/46.jpg',
+      title: 'Karma: The Dark World Demo',
+      price: 'Gratis',
+    },
+    {
+      img: 'images/gallery-carousel/47.jpg',
+      title: 'Pipistrello and the Cursed Yoyo',
+      price: 'Gratis',
+    },
+  ];
 }
