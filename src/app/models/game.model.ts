3dc163c9678771f9dc868a3fcd65795c329d4d11
salvobono <salvatorebono2001@gmail.com>
2025-04-01 14:11:32 +0200

    create model

---
+++ src/app/models/game.model.ts
@@ -0,0 +1,23 @@
+export interface Game {
+  id: number;
+  title: string;
+  price: string;
+  description: string;
+  img: string;
+}
+
+/*   edition: [
+    {
+      title: string;
+      price: string;
+      features: string[];
+      img: string;
+    }
+  ];
+  additional_content: [
+    {
+      title: string;
+      price: string;
+      img: string;
+    }
+  ]; */
