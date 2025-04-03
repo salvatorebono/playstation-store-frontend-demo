68623e05135ea5e71f3282417d0cd0cbf6f77217
salvobono <salvatorebono2001@gmail.com>
2025-04-03 20:39:09 +0200

    add additiona-content img

--- src/app/game-detail/game-detail.component.ts
+++ src/app/game-detail/game-detail.component.ts
@@ -28,19 +28,50 @@ export class GameDetailComponent implements OnInit {
         img: 'images/edition/2.png',
       },
       {
-        title: 'Standard Edition',
-        features: ['EA SPORTS FC™ 25 per PS4 e PS5'],
-        price: 79.99,
-        img: 'images/edition/1.png',
+        title: 'EA SPORTS FC™ 25 SHOWCASE',
+        features: ['EA SPORTS FC™ 25 SHOWCASE'],
+        price: 0,
+        img: 'images/edition/2.png',
       },
       {
-        title: 'Standard Edition',
-        features: ['EA SPORTS FC™ 25 per PS4 e PS5'],
-        price: 79.99,
-        img: 'images/edition/1.png',
+        title: 'Ultimate Edition',
+        features: ['EA SPORTS FC™ 25 per PS4 e PS5', '4.600 FC Points'],
+        price: 109.99,
+        img: 'images/edition/3.png',
       },
     ],
-    additional_content: [{}],
+    additional_content: [
+      {
+        title: 'EA SPORTS FC™ 25 - 5.900 FC Points',
+        price: 49.99,
+        img: 'images/additional-content/1.jpg',
+      },
+      {
+        title: 'EA SPORTS FC™ 25 - 12.000 FC Points',
+        price: 99.99,
+        img: 'images/additional-content/2.jpg',
+      },
+      {
+        title: 'EA SPORTS FC™ 25 - 18.500 FC Points',
+        price: 149.99,
+        img: 'images/additional-content/3.jpg',
+      },
+      {
+        title: 'EA SPORTS FC™ 25 - 2.800 FC Points',
+        price: 24.99,
+        img: 'images/additional-content/4.jpg',
+      },
+      {
+        title: 'EA SPORTS FC™ 25 - 1.050 FC Points',
+        price: 9.99,
+        img: 'images/additional-content/5.jpg',
+      },
+      {
+        title: 'EA SPORTS FC™ 25 - 1.600 FC Points',
+        price: 14.99,
+        img: 'images/additional-content/6.jpg',
+      },
+    ],
   };
 
   constructor(
